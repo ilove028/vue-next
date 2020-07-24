423697ed35ed879ac41d105c781aff5ca67c23a6
ilove028 <maoxuheng@cnpc.com.cn>
2020-07-24 16:56:42 +0800

    feat: 增加vue 测试代码

---
+++ tutorial/vue/js/index.js
@@ -0,0 +1,56 @@
+function useAuotIncrement() {
+  const count = Vue.ref(0)
+  let i = 0
+  let interval = null
+
+  Vue.onMounted(() => {
+    i++
+    if (!interval) {
+      interval = setInterval(() => count.value++, 1000)
+    }
+  })
+
+  Vue.onBeforeUnmount(() => {
+    i--
+    if (i <= 0) {
+      clearInterval(interval)
+      interval = null
+    }
+  })
+
+  return { count }
+}
+
+const Counter = {
+  template: `<div>{{ count }}</div>`,
+  setup() {
+    const { count } = useAuotIncrement()
+    return {
+      count
+    }
+  }
+}
+
+const App = {
+  template: '<counter v-if="show" />',
+  data() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    toggle() {
+      setTimeout(() => {
+        this.show = !this.show
+        this.toggle()
+      }, 5000)
+    }
+  },
+  mounted() {
+    this.toggle()
+  }
+}
+
+Vue.createApp(App)
+  .component('counter', Counter)
+  .mount('#app')
