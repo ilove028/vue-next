6fe4f6f159aaa41021483db51271e47f94563d17
ilove028 <maoxuheng@cnpc.com.cn>
2021-10-01 16:17:49 +0800

    chore: 增加compile模块

--- tutorial/README.md
+++ tutorial/README.md
@@ -6,6 +6,7 @@
 [virtual DOM](https://segmentfault.com/a/1190000022442171)
 [Vue diff](https://www.jianshu.com/p/eb08022ee488)
 [Vue Compiler](https://zhuanlan.zhihu.com/p/150732926)
+[中文网站有源码解析相关库](https://www.vue3js.cn/reactivity/reactive.html)
 
 template-explorer
 https://template-explorer.vuejs.org 2.x
--- tutorial/compiler/js/index.js
+++ tutorial/compiler/js/index.js
@@ -1,1 +1,13 @@
-console.log(Vue.compile(document.getElementById('template').textContent))
+// console.log(Vue.compile(document.getElementById('template').textContent))
+;(function (Vue) {
+  const { createApp } = Vue
+  const App = {
+    template: `
+      <div>
+        <span>Static</span>
+      </div>
+    `
+  }
+
+  const vm = createApp(App).mount('#app')
+})(Vue)
