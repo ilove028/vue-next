cad10f5813965a6e890d8730301c34b511fbb308
ilove028 <maoxuheng@cnpc.com.cn>
2021-10-02 16:35:53 +0800

    chore: 增加compile

--- tutorial/README.md
+++ tutorial/README.md
@@ -6,6 +6,7 @@
 [virtual DOM](https://segmentfault.com/a/1190000022442171)
 [Vue diff](https://www.jianshu.com/p/eb08022ee488)
 [Vue Compiler](https://zhuanlan.zhihu.com/p/150732926)
+[Vue Compiler](https://blog.csdn.net/More_Action/article/details/109802319?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-1.no_search_link&spm=1001.2101.3001.4242)
 [中文网站有源码解析相关库](https://www.vue3js.cn/reactivity/reactive.html)
 
 template-explorer
--- tutorial/compiler/js/index.js
+++ tutorial/compiler/js/index.js
@@ -1,13 +1,68 @@
 // console.log(Vue.compile(document.getElementById('template').textContent))
 ;(function (Vue) {
-  const { createApp } = Vue
-  const App = {
+  const { createApp, ref, reactive, compile } = Vue
+  const Com1 = {
     template: `
       <div>
-        <span>Static</span>
+        <slot />
       </div>
-    `
+    `,
+    props: {
+      modelValue: {
+        type: String,
+        default: ''
+      }
+    },
+    setup(props, { emit }) {
+      return {
+        handleInput(e) {
+          emit('update:modelValue', e.target.value)
+        }
+      }
+    }
   }
+  const App = {
+    template: `
+      <div v-once>
+        <Com1>
+          <p>Slot</p>
+        </Com1>
+        <h1 v-if="title">{{ name }}</h1>
+        <span v-for="s of students" :key="s">
+          {{ s }}
+        </span>
+      </div>
+    `,
+    components: { Com1 },
+    setup() {
+      const refTitle = ref(true)
+      const refName = ref('YY')
+      const retStudents = reactive([])
 
+      setInterval(() => {
+        refTitle.value = !refTitle.value
+        retStudents.push(Math.random().toString(16).substr(2))
+      })
+      return {
+        title: refTitle,
+        students: retStudents,
+        name: refName,
+        handleClick() {}
+      }
+    }
+  }
+  console.log(
+    compile(
+      `
+        <div>
+          <span>1</span>
+        </div>
+      `,
+      {
+        // hoistStatic: true
+      }
+    )
+  )
   const vm = createApp(App).mount('#app')
+  console.log(vm, App, Com1)
 })(Vue)
